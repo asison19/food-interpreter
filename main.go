d450cbc741569abd4e4d33a696682490692eae29
asison19 <andrewsison19@gmail.com>
2024-10-29 20:53:45 -0700

    Add file scanning

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	args := os.Args[1:]
+
+	// TODO command line args
+	scanFile(args[0])
+}
+
+func scanFile(filePath string) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+	}
+}
