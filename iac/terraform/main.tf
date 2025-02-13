47e584591f73868c287255d2c026eb2275c9658b
asison19 <andrewsison19@gmail.com>
2025-02-12 21:01:29 -0800

    feat(iac): add GCR repository

--- iac/terraform/main.tf
+++ iac/terraform/main.tf
@@ -9,3 +9,10 @@ resource "google_pubsub_topic" "lexer" {
 
   message_retention_duration = "86600s"
 }
+
+resource "google_artifact_registry_repository" "food-interpreter-repository" {
+  location      = "us-central1"
+  repository_id = "food-interpreter-repository"
+  description   = "Food Interpreter docker repository"
+  format        = "DOCKER"
+}
