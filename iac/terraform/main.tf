dd5b229b62bbd58a55456cb81e1667917adaee18
asison19 <andrewsison19@gmail.com>
2025-02-14 20:40:52 -0800

    chore(iac): remove unused pub sub topic

--- iac/terraform/main.tf
+++ iac/terraform/main.tf
@@ -1,15 +1,6 @@
 // TODO GKE
 
-resource "google_pubsub_topic" "lexer" {
-  name = "food-interpreter-lexer"
-
-  labels = {
-    application = "food-interpreter"
-  }
-
-  message_retention_duration = "86600s"
-}
-
+# TODO look into automatically deleting images.
 resource "google_artifact_registry_repository" "food-interpreter-repository" {
   location      = "us-central1"
   repository_id = "food-interpreter-repository"
