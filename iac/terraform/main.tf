a528886759de5ebafcbde93c795ef89ea730cdd5
asison19 <andrewsison19@gmail.com>
2025-02-24 20:16:40 -0800

    feat(cloudrun): Add GCP cloud run job for the lexer image

--- iac/terraform/main.tf
+++ iac/terraform/main.tf
@@ -15,3 +15,44 @@ resource "google_artifact_registry_repository" "food-interpreter-repository" {
     }
   }
 }
+
+resource "google_cloud_run_v2_job" "lexer" {
+  name     = "lexer-cloud-run"
+  location = "us-central1"
+  deletion_protection = false
+
+  template {
+    template {
+      timeout = "10s"
+      containers {
+        image = "us-central1-docker.pkg.dev/${ var.GCP_PROJECT_ID }/${ google_artifact_registry_repository.food-interpreter-repository.name }/food-interpreter:latest"
+        resources {
+          limits = {
+            memory = "1024Mi"
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "google_service_account" "lexer_cloud_run" {
+  account_id   = "lexer-cloud-run"
+  display_name = "Lexer Cloud Run Service Account"
+}
+
+data "google_iam_policy" "lexer" {
+  binding {
+    role = "roles/viewer" # TODO give it the roles it needs later on.
+    members = [
+      google_service_account.lexer_cloud_run.email,
+    ]
+  }
+}
+
+resource "google_cloud_run_v2_job_iam_policy" "lexer" {
+  project     = google_cloud_run_v2_job.lexer.project
+  location    = google_cloud_run_v2_job.lexer.location
+  name        = google_cloud_run_v2_job.lexer.name
+  policy_data = data.google_iam_policy.lexer.policy_data
+}
