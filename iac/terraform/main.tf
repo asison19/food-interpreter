77d8f140cb903152697481fc546e05c10e90acfc
asison19 <andrewsison19@gmail.com>
2025-03-24 15:53:46 -0700

    fix(cloudrun): fix not using latest image by using the git shortened sha instead of latest tag

--- iac/terraform/main.tf
+++ iac/terraform/main.tf
@@ -28,7 +28,7 @@ resource "google_cloud_run_v2_service" "lexer" {
   template {
     timeout = "10s"
     containers {
-      image = "${ var.GCP_PROJECT_REGION }-docker.pkg.dev/${ var.GCP_PROJECT_ID }/${ google_artifact_registry_repository.food-interpreter-repository.name }/food-interpreter:latest"
+      image = "${ var.GCP_PROJECT_REGION }-docker.pkg.dev/${ var.GCP_PROJECT_ID }/${ google_artifact_registry_repository.food-interpreter-repository.name }/food-interpreter:${ var.SHA_SHORT }"
       resources {
         limits = {
           memory = "1024Mi"
--- iac/terraform/vars.tf
+++ iac/terraform/vars.tf
@@ -3,8 +3,15 @@ variable "GCP_PROJECT_ID" {
     default = ""
     description = "GCP Project ID of where to create the infrastructure."
 }
+
 variable "GCP_PROJECT_REGION" {
     type = string
     default = ""
     description = "GCP Project region of where to create the infrastructure."
+}
+
+variable "SHA_SHORT" {
+    type = string
+    default = ""
+    description = "Git SHA shortened used to use the latest image for Cloud Run."
 }
