0552241537e4594a0f965d2fbe8b82ec17efb6f1
asison19 <andrewsison19@gmail.com>
2025-04-18 19:31:16 -0700

    feat(parser): add initial parser, parsing year root

--- parser/parser.go
+++ parser/parser.go
@@ -1,59 +1,79 @@
 package parser
 
 import (
-	"bufio"
 	"fmt"
 	"food-interpreter/lexer"
-	"log"
-	"os"
-	"regexp"
-	"strconv"
-	"strings"
 )
-
-func ParseFile(filePath string) {
-	file, err := os.Open(filePath)
-	if err != nil {
-		log.Println(err)
-		return Lexer{}
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	return Parse(scanner * bufio.Scanner)
-}
-
-func Parse(scanner *bufio.Scanner) {
-	for scanner.Scan() {
-		line := scanner.Text()
-
-	}
-}
 
 type Parser struct {
 	tokens  []lexer.Token
-	current int 
+	current lexer.Token
+	index   int
 }
 
-func newParser(tokens []lexer.Token) Parser {
-	return Parser{
-		tokens = tokens,
-		current = 0,
+// TODO newline seperation? Lexer needs to deal with it if so?
+// TODO lexer.Token, is there a better way of doing this?
+
+func ParseTokens(tokens []lexer.Token) Parser {
+	parser := Parser{tokens, tokens[0], 0}
+
+	e := parser.parse()
+	fmt.Print(e)
+
+	return parser // TODO what exactly should be returned?
+}
+
+func (p *Parser) parse() int {
+	for p.index < len(p.tokens) {
+		p.year() // TODO year isn't the only root
+		fmt.Println()
 	}
+	return 0
 }
 
-func year() Year {
-	match()
-	return Year
+// Go to the next token
+func (p *Parser) nextToken() bool {
+	// TODO check for last
+	p.index++
+	if p.index < len(p.tokens) {
+		p.current = p.tokens[p.index]
+		return true
+	}
+	return false
 }
 
-func semicolon() {
-
+// Consume the next token if it's acceptable
+func (p *Parser) accept(tokenType int) bool {
+	if p.current.TokenType == tokenType {
+		fmt.Printf("%v accepted", p.current.TokenType) // TODO print the token type in string not int
+		p.nextToken()
+		return true
+	}
+	return false
 }
 
-type YearNt interface {
-
+// Check the next token is as expected
+func (p *Parser) expect(tokenType int) bool {
+	if p.accept(tokenType) {
+		return true
+	}
+	print("Error: unexpected symbol")
+	return false
 }
+
+func (p *Parser) year() {
+	p.accept(lexer.YEAR)
+	p.semicolon()
+}
+
+//
+func (p *Parser) semicolon() {
+	p.accept(lexer.SEMICOLON)
+}
+
+//
+//type Yr interface {
+//}
 
 // TODO tree nodes should be structs?
 type Year struct {
