ea20fe680a1a2c4d338eca961cbeff8fab9983a7
asison19 <andrewsison19@gmail.com>
2025-04-20 15:59:03 -0700

    test(parser): Add initial parser testing files

---
+++ cmd/parser/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"food-interpreter/lexer"
+	"food-interpreter/parser"
+)
+
+func main() {
+	//lex := lexer.LexFile("test/data/year.txt")
+	lex := lexer.LexFile("test/data/test.txt")
+
+	par := parser.ParseTokens(lex.Tokens)
+	fmt.Print(par)
+}
