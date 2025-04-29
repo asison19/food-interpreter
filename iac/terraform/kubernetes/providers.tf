84c51a283d187db47b7eb6c3021583eda59f8982
asison19 <andrewsison19@gmail.com>
2025-04-28 20:04:52 -0700

    fix(kubernetes): fix reference to proper google_container_cluster resource

--- iac/terraform/kubernetes/providers.tf
+++ iac/terraform/kubernetes/providers.tf
@@ -4,7 +4,7 @@ provider "google" {
 }
 
 provider "kubernetes" {
-  host = "https://${data.google_container_cluster.primary.endpoint}"
+  host = "https://${ google_container_cluster.primary.endpoint }"
   token = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
 
