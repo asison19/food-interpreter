b7c31bca0a1cb1b0ad0b511e1729cc96718b3088
asison19 <andrewsison19@gmail.com>
2025-04-29 15:23:48 -0700

    fix(kubernetes): add networking resources and details

--- iac/terraform/kubernetes/kubernetes-cluster.tf
+++ iac/terraform/kubernetes/kubernetes-cluster.tf
@@ -2,10 +2,40 @@
 # Creating the Kubernetes cluster
 #
 
+# TODO can I remove this?
 resource "google_service_account" "default" {
   account_id   = "gke-service-account"
   display_name = "Service Account"
 }
+
+resource "google_compute_network" "default" {
+  name = "primary-network"
+
+  auto_create_subnetworks  = false
+  enable_ula_internal_ipv6 = true
+}
+
+resource "google_compute_subnetwork" "default" {
+  name = "primary-subnetwork"
+
+  ip_cidr_range = "10.0.0.0/16"
+  region        = "us-central1"
+
+  stack_type       = "IPV4_IPV6"
+  ipv6_access_type = "INTERNAL" # Change to "EXTERNAL" if creating an external loadbalancer
+
+  network = google_compute_network.default.id
+  secondary_ip_range {
+    range_name    = "services-range"
+    ip_cidr_range = "192.168.0.0/24"
+  }
+
+  secondary_ip_range {
+    range_name    = "pod-ranges"
+    ip_cidr_range = "192.168.1.0/24"
+  }
+}
+
 
 # TODO rename and move these resources? Can't move, immutable name
 resource "google_container_cluster" "primary" {
@@ -16,9 +46,16 @@ resource "google_container_cluster" "primary" {
   #remove_default_node_pool = true
   initial_node_count       = 1
   enable_autopilot         = true # TODO keep autopilot?
+  enable_l4_ilb_subsetting = true
 
   deletion_protection = false
 
-  #network    = google_compute_network.default.id
-  #subnetwork = google_compute_subnetwork.default.id
+  network    = google_compute_network.default.id
+  subnetwork = google_compute_subnetwork.default.id
+
+  ip_allocation_policy {
+    stack_type                    = "IPV4_IPV6"
+    services_secondary_range_name = google_compute_subnetwork.default.secondary_ip_range[0].range_name
+    cluster_secondary_range_name  = google_compute_subnetwork.default.secondary_ip_range[1].range_name
+  }
 }
