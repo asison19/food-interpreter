d48a08b4ac6e6be239aa61ea79fa8f8ab0661bfe
asison19 <andrewsison19@gmail.com>
2025-05-05 19:09:42 -0700

    WIP: parser testing

---
+++ parser/parser_test.go
@@ -0,0 +1,14 @@
+package parser
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestParserYear(t * testing.T) {
+	expected := [] {}
+}
+
+func TestParserMonthAndYear(t * testing.T) {
+	expected := [] {}
+}
