5e5349d8442deb688e5bea9cb8a4a05dc3f56b74
asison19 <andrewsison19@gmail.com>
2025-05-16 20:51:00 -0700

    fix(versioning): escape $ for version

--- scripts/generate-release.sh
+++ scripts/generate-release.sh
@@ -2,8 +2,8 @@
 
 BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
 if [ "$BRANCH_NAME" != "master" ]; then
-  TAG_FORMAT="${BRANCH_NAME}-v${version}"
+  TAG_FORMAT="v\${version}-${BRANCH_NAME}"
 else
-  TAG_FORMAT="v${version}"
+  TAG_FORMAT="v\${version}"
 fi
 npx semantic-release --tag-format "${TAG_FORMAT}"
