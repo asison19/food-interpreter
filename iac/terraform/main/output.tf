20c57922fc9d17ab5de5934bc3537c2efd7b1664
asison19 <andrewsison19@gmail.com>
2025-06-03 20:14:14 -0700

    feat(gateway): add interpreter cloud run service URI to gateway docker image

--- iac/terraform/main/output.tf
+++ iac/terraform/main/output.tf
@@ -1,3 +1,7 @@
 output "interpreter_pubsub_topic_id" {
     value = google_pubsub_topic.interpreter.id
 }
+
+output "interpreter_cloud_run_uri" {
+    value = google_cloud_run_v2_service.interpreter.uri
+}
