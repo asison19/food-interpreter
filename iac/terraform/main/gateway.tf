c066785ac32ec6feee14f2e1d54db72363b52d42
asison19 <andrewsison19@gmail.com>
2025-06-29 11:50:29 -0700

    feat(grpc): add iam run.invoker for gateway for interpreter_grpc

--- iac/terraform/main/gateway.tf
+++ iac/terraform/main/gateway.tf
@@ -29,6 +29,13 @@ resource "google_cloud_run_service_iam_binding" "gateway_servicesinvoker" {
   members  = ["serviceAccount:${ google_service_account.gateway_cloud_run.email }"]
 }
 
+resource "google_cloud_run_service_iam_binding" "gateway_interpreter_grpc_servicesinvoker" {
+  location = google_cloud_run_v2_service.interpreter_grpc.location
+  service  = google_cloud_run_v2_service.interpreter_grpc.name
+  role     = "roles/run.invoker"
+  members  = ["serviceAccount:${ google_service_account.gateway_cloud_run.email }"]
+}
+
 # TODO, this keeps getting the service account rewritten.
 resource "google_project_iam_binding" "gateway_logwriter" {
   project = var.GCP_PROJECT_ID
