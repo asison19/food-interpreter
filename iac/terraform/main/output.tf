e73f157556da3fdf936e32a21a9faa4d3d7a1cd4
asison19 <andrewsison19@gmail.com>
2025-06-30 13:51:13 -0700

    Revert "fix(grpc): add urls[0] for output for interpreter_grpc cloud run service"
    This reverts commit 06f5574f38331a205984a853b3340645b5a69141.

--- iac/terraform/main/output.tf
+++ iac/terraform/main/output.tf
@@ -3,5 +3,5 @@ output "interpreter_pubsub_topic_id" {
 }
 
 output "interpreter_grpc_cloud_run_uri" {
-    value = google_cloud_run_v2_service.interpreter_grpc.urls[0]
+    value = google_cloud_run_v2_service.interpreter_grpc.uri
 }
