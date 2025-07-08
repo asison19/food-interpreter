a58ede9ea4a5e5f34808068892cf9ca0516b5072
asison19 <andrewsison19@gmail.com>
2025-07-07 21:27:08 -0700

    fix(grpc): add service account key, add copying file GOOGLE_APPLICATION_CREDENTIALS to gateway dockerfile image

--- iac/terraform/main/output.tf
+++ iac/terraform/main/output.tf
@@ -5,3 +5,10 @@ output "interpreter_pubsub_topic_id" {
 output "interpreter_grpc_cloud_run_uri" {
     value = google_cloud_run_v2_service.interpreter_grpc.uri
 }
+
+# This is only populated when creating a new key.
+# Is this necessary? The ID token should be made in interpretRequestWithAuth().
+output "interpreter_grpc_sa_key" {
+    value = google_service_account_key.interpeter_grpc.private_key
+    sensitive = true
+}
