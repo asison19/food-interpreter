ba7802596810c212a002fa0c05df995670bcd561
asison19 <andrewsison19@gmail.com>
2025-07-27 20:04:12 -0700

    test(grpc): use gateway service account

--- iac/terraform/main/output.tf
+++ iac/terraform/main/output.tf
@@ -9,3 +9,7 @@ output "interpreter_grpc_cloud_run_uri" {
 output "gateway_cloud_run_uri" {
     value = google_cloud_run_v2_service.gateway.uri
 }
+
+output "gateway_service_account" {
+    value = google_service_account.gateway_cloud_run.email
+}
