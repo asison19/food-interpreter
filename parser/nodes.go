b6b15427740e178f6acd889d5cdaceb181450414
asison19 <andrewsison19@gmail.com>
2025-09-20 18:00:01 -0700

    feat(parser): add getToken to Node interface

--- parser/nodes.go
+++ parser/nodes.go
@@ -4,11 +4,17 @@ import (
 	"food-interpreter/lexer"
 )
 
-type Node interface{}
+type Node interface {
+	GetToken() lexer.Token
+}
 
 type Year struct {
 	year      lexer.Token
 	semicolon Semicolon
+}
+
+func (y Year) GetToken() lexer.Token {
+	return y.year
 }
 
 type Semicolon struct {
@@ -16,9 +22,17 @@ type Semicolon struct {
 	time      Time
 }
 
+func (s Semicolon) GetToken() lexer.Token {
+	return s.semicolon
+}
+
 type MonthAndDay struct {
 	monthAndDay lexer.Token
 	time        Time
+}
+
+func (m MonthAndDay) GetToken() lexer.Token {
+	return m.monthAndDay
 }
 
 type Time struct {
@@ -26,9 +40,17 @@ type Time struct {
 	right Node // (<food> | <repeater> | <sleep>)
 }
 
+func (t Time) GetToken() lexer.Token {
+	return t.time
+}
+
 type Food struct {
 	food  lexer.Token
 	right Node // (<comma> | <semicolon>)
+}
+
+func (f Food) GetToken() lexer.Token {
+	return f.food
 }
 
 type Comma struct {
@@ -36,12 +58,24 @@ type Comma struct {
 	food  Food
 }
 
+func (c Comma) GetToken() lexer.Token {
+	return c.comma
+}
+
 type Repeater struct {
 	repeater lexer.Token
 	right    Node // (<comma> | <semicolon>)
 }
 
+func (r Repeater) GetToken() lexer.Token {
+	return r.repeater
+}
+
 type Sleep struct {
 	sleep lexer.Token
 	right Node // (<comma> | <semicolon>)
+}
+
+func (s Sleep) GetToken() lexer.Token {
+	return s.sleep
 }
