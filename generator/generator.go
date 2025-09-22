4b903e70d674ebfa0305ea5de0b20a3e0903fe85
asison19 <andrewsison19@gmail.com>
2025-09-21 20:11:54 -0700

    feat(generator): initial start of generator, add year handling.

--- generator/generator.go
+++ generator/generator.go
@@ -6,18 +6,56 @@ package generator
 import (
 	"fmt"
 	"food-interpreter/parser"
+	"log"
+	"strconv"
+	"time"
+	"unicode/utf8"
 )
+
+// TODO make this configurable
+var timezone time.Location = *time.Local
+
+// Current working date
+type currentDate struct {
+	year  int
+	month time.Month
+	day   int
+}
 
 // Nodes - slice of root nodes (YEAR or MONTHANDDAY)
 func Generate(nodes []parser.Node) {
 	fmt.Printf("Nodes: %+v\n", nodes)
 	for _, node := range nodes {
-		if _, ok := node.(parser.Year); ok {
+
+		// TODO switch?
+		if yearNode, ok := node.(parser.Year); ok {
 			fmt.Println("YEAR node")
+			year := handleYear(yearNode)
+			fmt.Printf("The working year is %d\n", year)
 		}
+
+		// TODO, the rest of the tokens
+
+		// In the case of semicolon, do nothing
+
 		fmt.Println(node)
 	}
 }
 
-//func handleYear(node []parser.Node) {
-//}
+// Given a year node, return the int value of the year.
+func handleYear(yearNode parser.Year) int {
+	token := yearNode.GetToken()
+	number, err := strconv.Atoi(trimFirstRune(token.Lexeme))
+	if err != nil {
+		log.Fatal("Year token is not an int: " + token.Lexeme)
+	}
+
+	return number
+}
+
+// Send the result of removing the first element in the passed in string
+func trimFirstRune(s string) string {
+	_, i := utf8.DecodeRuneInString(s)
+	fmt.Println(i)
+	return s[i:]
+}
