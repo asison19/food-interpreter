4d5582892956a7c8eb6813a783f3aabde0ff0f9b
asison19 <andrewsison19@gmail.com>
2025-09-28 20:37:30 -0700

    feat(interpreter): output generated diary to local file

--- interpreter/interpreter.go
+++ interpreter/interpreter.go
@@ -4,6 +4,9 @@ import (
 	"food-interpreter/generator"
 	"food-interpreter/lexer"
 	"food-interpreter/parser"
+	"os"
+	"strings"
+	"time"
 )
 
 // TODO return the parser for now
@@ -11,7 +14,30 @@ func Interpret(diary string) parser.Parser {
 
 	l := lexer.LexString(diary)
 	p, nodes := parser.ParseTokens(l.Tokens)
-	generator.Generate(nodes)
 
-	return p
+	entries := generator.Generate(nodes)
+	writeToFile(entries)
+
+	return p // TODO return diary entries
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+// TODO work on pandas/visualizer next to determine what the output should be
+
+// local file output for testing
+func writeToFile(entries []generator.DiaryEntry) {
+	now := time.Now()
+	f, err := os.Create("diaryc-" + now.Format("20060102-150405"))
+	check(err)
+
+	for _, e := range entries {
+		f.WriteString(e.Date.String() + ", " + strings.Join(e.List, ", ") + "\n")
+	}
+
+	defer f.Close()
 }
